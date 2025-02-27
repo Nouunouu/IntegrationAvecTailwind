4da758ac9f4425e6c8c6d0f40b7c8e563e4d7535
Nourchenenba <nourchene.benabdallah@esprit.tn>
2025-02-27 14:45:25 +0100

    schelletteProjet

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#B34532",
+        "background-color": "#171717",
+
+    },
+  },
+  plugins: [],
+}
+
+}
